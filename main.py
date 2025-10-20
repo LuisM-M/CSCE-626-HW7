84a82e6fd1485530ffc81f08f274a8f8de778981
Luis Martinez <2002luismm@gmail.com>
2025-10-20 01:10:07 -0500

    Practice with heapq

--- main.py
+++ main.py
@@ -1,2 +1,25 @@
-name = "Hello World!"
-print(name)
+import heapq
+# binary min heap implementation
+
+# practice with heapq
+he = []
+heapq.heappush(he, 1)
+
+print(heapq.heappop(he))
+
+
+list1 = [5,4,3,2,10, 9, 22]
+
+print(list1)
+
+heapq.heapify(list1)
+
+print(list1)
+
+heapq.heappop(list1)
+
+print(list1)
+
+
+while len(list1) > 1:
+    print(heapq.heappop(list1))

36b9e6a22a29a954e9d223dbf904454cb4ba8a86
Luis Martinez <2002luismm@gmail.com>
2025-10-21 20:23:20 -0500

    Small test case

--- dijkstras.py
+++ dijkstras.py
@@ -22,3 +22,58 @@ print("Distances: ", d)
 print("Parents: ", parent)
 print("Priority Queue: ", Q)
 
+print()
+print("first iteration ############")
+
+# u:= extract-min(Q)
+
+current_dist, u = heapq.heappop(Q)
+
+print("Popped", u, "with distance", current_dist,"from S")
+print("Q after pop:", Q)
+
+print("Relaxing neighbors of S")
+for v, weight in map[u].items():
+    if d[u] + weight < d[v]:
+        print("\tUpdating", v, "with distance:", d[v])
+        print("\twith NEW distance of:" , d[u] + weight)
+        print()
+        d[v] = d[u] + weight
+        parent[v] = u
+        heapq.heappush(Q, (d[v], v))
+
+
+print("Distances: ", d)
+print("Parents: ", parent)
+print("Priority Queue: ", Q)
+print()
+
+# popping A and relaxation of neighbors
+
+print("Popping A and proper steps ############")
+current_dist, u = heapq.heappop(Q)
+
+print("Popped", u, "with distance", current_dist,"from S")
+print("Q after pop:", Q)
+
+print("Relaxing neighbors of A")
+for v, weight in map[u].items():
+    if v ==s:
+        continue
+    if d[u] + weight < d[v]:
+        print("\tUpdating", v, "with distance:", d[v])
+        print("\twith NEW distance of:" , d[u] + weight)
+        print()
+        d[v] = d[u] + weight
+        parent[v] = u
+        heapq.heappush(Q, (d[v], v))
+    else:
+        print("No update for", v, "path through A is not better than current")
+
+
+print("Distances: ", d)
+print("Parents: ", parent)
+print("Priority Queue: ", Q)
+print()
+
+
